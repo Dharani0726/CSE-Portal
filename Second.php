f2f914ae081238fdea8b02e467641adf01d1b743
Dharani0726 <63237584+Dharani0726@users.noreply.github.com>
2020-08-07 21:05:07 +0530

    Add files via upload

---
+++ Second.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  	<title>2nd Year Study Materials</title>
+  	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<br>
+		<div class="panel panel-default">
+			<div class="panel-heading"><h3><b><center>SECOND YEAR</h3></b></div>
+      		<div class="panel-body">
+        		<div class="row">
+          			<div class="col-sm-4">
+            		<h5 style="font-size:18px" "font-family:TimesNewRoman">
+              		<ul>
+                		<li><h3><b>1st Semester</b></h3>
+                  		<ul><br>
+                    		<li><b>BEE Study Material</b>
+                      		<ul><br>
+                        		<li><a href="PDFs/2-1/BEE/BEE Unit-1.1.pdf">Unit-1.1</a></li>
+                        		<li><a href="PDFs/2-1/BEE/BEE Unit-1.2.pdf">Unit-1.2</a></li>
+	                        	<li><a href="PDFs/2-1/BEE/BEE Unit-2.1.pdf">Unit-2.1</a></li>
+    	                    	<li><a href="PDFs/2-1/BEE/BEE Unit-2.2.pdf">Unit-2.2</a></li>
+        	                	<li><a href="PDFs/2-1/BEE/BEE Unit-3.1.pdf">Unit-3.1</a></li>
+            	            	<li><a href="PDFs/2-1/BEE/BEE Unit-3.2.pdf">Unit-3.2</a></li>
+                	        	<li><a href="PDFs/2-1/BEE/BEE Unit-4.pdf">Unit-4</a></li>
+                    	    	<li><a href="PDFs/2-1/BEE/BEE Unit-5.pdf">Unit-5</a></li>
+                      		</ul>
+                    		</li><br>
+                    		<li><b>DM Study Material</b>
+                      		<ul><br>
+                        		<li><a href="PDFs/2-1/DM/DM Unit-1.pdf">Unit-1</a></li>
+                        		<li><a href="PDFs/2-1/DM/DM Unit-2.pdf">Unit-2</a></li>
+	                        	<li><a href="PDFs/2-1/DM/DM Unit-3.pdf">Unit-3</a></li>
+    	                    	<li><a href="PDFs/2-1/DM/DM Unit-4&5.pdf">Units-4 & 5</a></li>
+        	              	</ul>
+            	        	</li><br>
+                	    	<li><b>ES Study Material</b>
+                    	  	<ul><br>
+                        		<li><a href="PDFs/2-1/ES/ES Unit-1.pdf">Unit-1</a></li>
+                        		<li><a href="PDFs/2-1/ES/ES Unit-2.pdf">Unit-2</a></li>
+                        		<li><a href="PDFs/2-1/ES/ES Unit-3.pdf">Unit-3</a></li>
+                    				<li><a href="PDFs/2-1/ES/ES Unit-4.pdf">Unit-4</a></li>
+	                     			<li><a href="PDFs/2-1/ES/ES Unit-5.pdf">Unit-5</a></li>
+    	                			<li><a href="PDFs/2-1/ES/ES Unit(1-5).pdf">Professional Publications</a></li>
+        	           			</ul>
+          	        		</li><br>
+                	    	<li><b>LST Study Material</b>
+                    			<ul><br>
+                    				<li><a href="PDFs/2-1/LST/LST Unit-1.pdf">Unit-1</a></li>
+                    				<li><a href="PDFs/2-1/LST/LST-Registers & Counters.pdf">Registers & Counters</a	></li>
+                    				<li><a href="PDFs/2-1/LST/LST Unit-1.pdf">Units-2,3,4,5</a></li>
+                  				</ul>
+                  			</li><br>
+                  		</ul>
+                  	</li>
+                  </ul>
+                </h5>
+                </div>
+                <div class="col-sm-4">
+                <h5 style="font-size:18px" "font-family:TimesNewRoman">
+                  <ul>
+              		  <li><h3><b>2nd Semester</b></h3>
+                		  <ul><br>
+                  			<li><b>CO Study Material</b>
+                    			<ul><br>
+                      			<li><a href="PDFs/2-2/CO/CO Unit-1&2.pdf">Units-1&2</a></li>
+                            <li><a href="PDFs/2-2/CO/CO Unit-3.pdf">Unit-3</a></li>
+                            <li><a href="PDFs/2-2/CO/CO Unit-4.pdf">Unit-4</a></li>
+                            <li><a href="PDFs/2-2/CO/CO Unit-5.pdf">Unit-5</a></li>
+                    			</ul>
+                  			</li><br>
+                  			<li><b>JAVA Study Material</b>
+                    			<ul><br>
+                      			<li><a href="PDFs/2-2/JAVA/JAVA Unit-1.pdf">Unit-1</a></li>
+                            <li><a href="PDFs/2-2/JAVA/JAVA Unit-2.pdf">Unit-2</a></li>
+                            <li><a href="PDFs/2-2/JAVA/JAVA Unit-3.pdf">Unit-3</a></li>
+                            <li><a href="PDFs/2-2/JAVA/JAVA Unit-4.pdf">Unit-4</a></li>
+                            <li><a href="PDFs/2-2/JAVA/JAVA Unit-5.pdf">Unit-5</a></li>
+                    			</ul>
+                  			</li><br>
+                  			<li><b>MP Study Material</b>
+                    			<ul><br>
+                      			<li><a href="PDFs/2-2/MP/MP Unit-1.pdf">Unit-1</a></li>
+                      			<li><a href="PDFs/2-2/MP/MP Unit-2.pdf">Unit-2</a></li>
+                    			</ul>
+                  			</li><br>
+                  			<li><b>PL Study Material</b>
+                    			<ul><br>
+                      			<li><a href="PDFs/2-2/PL/PL Unit-1.pdf">Unit-1</a></li>
+                        		<li><a href="PDFs/2-2/PL/PL Unit-2.pdf">Unit-2</a></li>
+                            <li><a href="PDFs/2-2/PL/PL Unit-3.pdf">Unit-3</a></li>
+                            <li><a href="PDFs/2-2/PL/PL Unit-4.pdf">Unit-4</a></li>
+                            <li><a href="PDFs/2-2/PL/PL Unit-5.pdf">Unit-5</a></li>
+                    			</ul>
+                  			</li><br>
+                  			<li><b>S&S Study Material</b>
+                    			<ul><br>
+                    				<li><a href="PDFs/2-2/S&S/S&S Unit-1.pdf">Unit-1</a></li>
+                    				<li><a href="PDFs/2-2/S&S/S&S Unit-2.pdf">Unit-2</a></li>
+                            <li><a href="PDFs/2-2/S&S/S&S Unit-3.pdf">Unit-3</a></li>
+                            <li><a href="PDFs/2-2/S&S/S&S Unit-4.pdf">Unit-4</a></li>
+                            <li><a href="PDFs/2-2/S&S/S&S Unit-5.pdf">unit-5</a></li>
+                          </ul>
+                        </li><br>
+                      </ul>
+                    </li>
+                  </ul>
+                </h5>
+                </div>
+                <div class="col-sm-4">
+                  <br><br><br><br>
+                  <img src="StudyMaterials.png">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+</body>
+</html>
